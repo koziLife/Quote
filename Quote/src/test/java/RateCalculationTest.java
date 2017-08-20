589bd3e3068fa62d8a45fcf462bbaa2ac2d9a6ec
meryem <meryemalay@gmail.com>
2017-08-20 22:40:37 +0100

    rate calculation first commit

---
+++ Quote/src/test/java/RateCalculationTest.java
@@ -0,0 +1,58 @@
+import static org.junit.Assert.assertEquals;
+
+public class RateCalculationTest {
+
+    private RateCalculation rateCalculation;
+
+    @org.junit.Before
+    public void setup(){
+        MockData mockData = new MockData();
+        rateCalculation = new RateCalculation(mockData.mockMarket());
+    }
+
+    @org.junit.Test(expected = IllegalArgumentException.class)
+    public void whenGivenEmptyFileName_ThanThrowException() throws Exception{
+        rateCalculation.quote(100, 36);
+    }
+
+    @org.junit.Test(expected = IllegalArgumentException.class)
+    public void whenGivenAmountLessThan1000_ThanThrowException() throws Exception{
+        rateCalculation.quote(100, 36);
+    }
+
+    @org.junit.Test(expected = IllegalArgumentException.class)
+    public void whenGivenAmountBiggerThan15000_ThanThrowException() throws Exception{
+        rateCalculation.quote(20000, 36);
+    }
+
+    @org.junit.Test(expected = IllegalArgumentException.class)
+    public void whenGivenAmountNotIncrement100_ThanThrowException() throws Exception{
+        rateCalculation.quote(1234, 36);
+    }
+
+    @org.junit.Test
+    public void whenGivenAmountThatIsAvailableFromOneLender() throws Exception{
+        QuoteResponse response = rateCalculation.quote(1000, 36);
+        assertEquals(1000, response.getRequestedAmount(),0);
+        assertEquals(20.0, response.getRate(), 0);
+        double monthlyAmount = Double.parseDouble(String.format("%.2f",1200.0/36));
+        assertEquals(monthlyAmount, response.getMonthlyRepayment(),0);
+        assertEquals(1200, response.getTotalRepayment(),0);
+    }
+
+    @org.junit.Test
+    public void whenGivenAmountThatIsNotAvailableFromOneLender() throws Exception{
+        QuoteResponse response = rateCalculation.quote(2000, 36);
+        assertEquals(2000, response.getRequestedAmount(),0);
+        assertEquals(30.0, response.getRate(), 0);
+        double monthlyAmount = Double.parseDouble(String.format("%.2f",2600.0/36));
+        assertEquals(monthlyAmount, response.getMonthlyRepayment(),0);
+        assertEquals(2600, response.getTotalRepayment(),0);
+    }
+
+    @org.junit.Test(expected = RuntimeException.class)
+    public void whenGivenAmountThatIsNotEnoughAmount() throws Exception{
+        QuoteResponse response = rateCalculation.quote(12000, 36);
+
+    }
+}
