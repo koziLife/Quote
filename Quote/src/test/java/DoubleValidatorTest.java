ca790c7aa8084e0109fe1f5c96830eb562fc30bf
meryem <meryemalay@gmail.com>
2017-08-22 10:13:38 +0100

    deleted

--- Quote/src/test/java/DoubleValidatorTest.java
+++
@@ -1,9 +0,0 @@
-import org.junit.Assert;
-
-public class DoubleValidatorTest {
-
-    @org.junit.Test
-    public void whenGivenInvalidLoanAmount_ThanParseDoubleSafeReturnsFalse() throws Exception {
-        Assert.assertFalse(DoubleValidator.validate(""));
-    }
-}
